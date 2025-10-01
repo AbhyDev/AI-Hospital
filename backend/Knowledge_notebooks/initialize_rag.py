3825a92298a41543a73627e523d949583f930d4a
Abhyuday Sharma <abhyudaysharma665@gmail.com>
2025-10-01 15:02:24 +0530

    updated req

--- backend/Knowledge_notebooks/initialize_rag.py
+++ backend/Knowledge_notebooks/initialize_rag.py
@@ -3,7 +3,7 @@ from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_chroma import Chroma
 
 class VectorRAG:
-    model_name = "BAAI/bge-large-en-v1.5"
+    model_name = "BAAI/bge-small-en-v1.5"
     model_kwargs = {"device": "cpu"}
     encode_kwargs = {"normalize_embeddings": True}
     embedding_model = HuggingFaceEmbeddings(
